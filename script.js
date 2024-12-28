bb7046eb795791ca22988e8c99dbf227308aca62
Raymond Kenny <raymondkenny00@gmail.com>
2024-12-28 14:03:20 -0500

    12/28/23 update

--- script.js
+++ script.js
@@ -271,3 +271,20 @@ function showSlidesSpiceSquare(n) {
     slides[slideIndexSpiceSquare - 1].style.display = "block";
 }
 
+// Show/Hide Scroll-to-Top Button
+window.onscroll = function () {
+    const scrollToTopButton = document.getElementById("scrollToTop");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollToTopButton.style.display = "flex";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+};
+
+// Scroll to Top Function
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+
+
